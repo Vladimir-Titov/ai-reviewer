dbddf7eee344376f369760aab6c4189596615122
vtitov <v.v.titov94@gmail.com>
2025-01-23 20:03:48 +0300

    feat: added call mr diffs

--- src/client/gitlab.rs
+++ src/client/gitlab.rs
@@ -8,7 +8,16 @@ impl<'a> GitlabHttpClient<'a> {
         Self { token, base_url }
     }
 
-    pub fn get_merge_request_diff(&self) {}
-
-    pub fn get_merge_requests(&self) {}
+    pub fn get_merge_request_diff(&self, project_id: u16, mr_id: u16) -> Result<(), ureq::Error> {
+        let url: String = format!(
+            "{}/api/v4/projects/{project_id}/merge_requests/{mr_id}/diffs",
+            self.base_url,
+        );
+        let resp: serde_json::Value = ureq::get(&url)
+            .set("PRIVATE-TOKEN", self.token)
+            .call()?
+            .into_json()?;
+        println!("{}", resp);
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,7 @@ fn run() {
     let cfg: AppConfig = AppConfig::build();
     let gitlab_client: GitlabHttpClient =
         GitlabHttpClient::new(&cfg.gitlab_token, &cfg.gitlab_base_url);
-    println!("{} {}", cfg.gitlab_token, cfg.gitlab_base_url)
+    gitlab_client
+        .get_merge_request_diff(87, 609)
+        .expect("Some error");
 }
